3b4015cb9f5f547c8458426354e6c5c7d7bd6327
Gilbert Ngeno <ngenogilbert07@gmail.com>
2022-02-18 14:39:42 +0300

    initial commit

---
+++ src/main/java/com/musicplayerbackend/app/MusicPlayerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.musicplayerbackend.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicPlayerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicPlayerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musicplayerbackend/app/MusicPlayerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicplayerbackend.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicPlayerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
